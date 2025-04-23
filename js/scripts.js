7183edfccc920f7ad56652967535e386bcdec744
tonymcaron <tonymcaron@gmail.com>
2025-04-23 09:06:21 -0700

    Add pokemonList array
    Created 4 objects in pokemonList with keys for name, type, height, and weight.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: "Bulbasaur", type: ["grass", "poison"], height: 0.7, weight: 6.9},
+    {name: "Rattata", type: "normal", height: 0.3, weight: 3.5},
+    {name: "Arcanine", type: "fire", height: 1.9, weight: 155},
+    {name: "Weedle", type: ["bug", "poison"], height: 0.3, weight: 3.2}
+];

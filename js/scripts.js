2e4e06a330d54085d8352603c44d57164532be5a
tonymcaron <tonymcaron@gmail.com>
2025-04-28 13:57:47 -0700

    Created loop for name + height
    Includes conditional to identify taller Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
 let pokemonList = [
-    {name: "Bulbasaur", type: ["grass", "poison"], height: 0.7, weight: 6.9},
-    {name: "Rattata", type: "normal", height: 0.3, weight: 3.5},
-    {name: "Arcanine", type: "fire", height: 1.9, weight: 155},
-    {name: "Weedle", type: ["bug", "poison"], height: 0.3, weight: 3.2}
+    {
+        name: "Bulbasaur",
+        type: ["grass", "poison"],
+        height: 0.7,
+        weight: 6.9
+    },
+    {
+        name: "Rattata",
+        type: "normal",
+        height: 0.3,
+        weight: 3.5
+    },
+    {
+        name: "Arcanine",
+        type: "fire",
+        height: 1.9,
+        weight: 155
+    },
+    {
+        name: "Weedle",
+        type: ["bug", "poison"],
+        height: 0.3,
+        weight: 3.2
+    }
 ];
+
+let i = 0;
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+    if (pokemonList[i].height > 1){
+        document.write(' - Wow, that\'s big!' + '<br>');
+    }
+}

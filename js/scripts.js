fc66f7a05b01d7a52d41ecf2654365f91a94f428
tonymcaron <tonymcaron@gmail.com>
2025-05-05 13:10:35 -0700

    Update script.js with forEach method
    Runs pokemon name and height w/ message if > 1 using forEach method.  (Part 1: Exercise 1.5)

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,25 @@ let pokemonList = [
     }
 ];
 
-// Display the list of Pokemon w/ heights on the DOM via 'for' loop
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-    // Adds message if Pokemon's height exceeds 1
-    if (pokemonList[i].height > 1) {
-        document.write(' - Wow, that\'s big!');
+//Display the list of Pokemon w/ heights on the DOM via 'for' loop
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    //Adds message if Pokemon's height exceeds 1
+//     if (pokemonList[i].height > 1) {
+//         document.write(' - Wow, that\'s big!');
+//     }
+//     document.write("<br>");
+// }
+
+//display list of pokemon names and heights using forEach method
+function pokemonDetails(pokemon) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
+    
+    //adds message if pokemon's height exceeds 1
+    if (pokemon.height > 1) {
+        document.write(' - Wow, that\'s a big one!');
     }
     document.write("<br>");
-}
+    }
+
+    pokemonList.forEach(pokemonDetails);

00f35f95a568f93595b8f7f2f1da196e0de58c4c
tonymcaron <tonymcaron@gmail.com>
2025-05-05 15:45:37 -0700

    Created pokemonRepository IIFE
    Added 'add' and 'getAll' functions and updated forEach call

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,49 @@
-let pokemonList = [
-    {
-        name: "Bulbasaur",
-        type: ["grass", "poison"],
-        height: 0.7,
-        weight: 6.9
-    },
-    {
-        name: "Rattata",
-        type: "normal",
-        height: 0.3,
-        weight: 3.5
-    },
-    {
-        name: "Arcanine",
-        type: "fire",
-        height: 1.9,
-        weight: 155
-    },
-    {
-        name: "Weedle",
-        type: ["bug", "poison"],
-        height: 0.3,
-        weight: 3.2
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            type: ["grass", "poison"],
+            height: 0.7,
+            weight: 6.9
+        },
+        {
+            name: "Rattata",
+            type: "normal",
+            height: 0.3,
+            weight: 3.5
+        },
+        {
+            name: "Arcanine",
+            type: "fire",
+            height: 1.9,
+            weight: 155
+        },
+        {
+            name: "Weedle",
+            type: ["bug", "poison"],
+            height: 0.3,
+            weight: 3.2
+        }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+    };
+})();
 
 //Display the list of Pokemon w/ heights on the DOM via 'for' loop
 // for (let i = 0; i < pokemonList.length; i++) {
 //     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-    //Adds message if Pokemon's height exceeds 1
+//Adds message if Pokemon's height exceeds 1
 //     if (pokemonList[i].height > 1) {
 //         document.write(' - Wow, that\'s big!');
 //     }
@@ -38,12 +53,12 @@ let pokemonList = [
 //display list of pokemon names and heights using forEach method
 function pokemonDetails(pokemon) {
     document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
-    
+
     //adds message if pokemon's height exceeds 1
     if (pokemon.height > 1) {
         document.write(' - Wow, that\'s a big one!');
     }
     document.write("<br>");
-    }
+}
 
-    pokemonList.forEach(pokemonDetails);
+document.write(pokemonRepository.getAll().forEach(pokemonDetails));

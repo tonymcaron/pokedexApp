909a99d326c3aeb884c0fff4969bb1b6c1c5370e
tonymcaron <tonymcaron@gmail.com>
2025-05-12 13:10:59 -0700

    Revert "Add h1 and ul to index.html"
    This reverts commit 6ebbe225ea5af66c6b9099e9e9cec92f84773057.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-//Pokemon repository with IIFE to store and manage Pokemon list
 let pokemonRepository = (function () {
     let pokemonList = [
         {
@@ -28,77 +26,19 @@ let pokemonRepository = (function () {
         }
     ];
 
-    //Function to add Pokemon to the list
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
 
-    ///Function to get all Pokemon from the list
     function getAll() {
         return pokemonList;
-    }
-
-    //Function to add Pokemon to the list
-    function addListItem(pokemon) {
-        //Select unordered list
-        let pokemonList = document.querySelector('.pokemon-list');
-
-        //Create list item
-        let listPokemon = document.createElement('li');
-
-        //Create button
-        let button = document.createElement('button');
-
-        //set button text to Pokemon name
-        button.innerText = pokemon.name;
-
-        //Add button class for styling
-        button.classList.add("button-class");
-
-        //Append button to list item
-        listPokemon.appendChild(button);
-
-        //Append list item to unordered list
-        pokemonList.appendChild(listPokemon);
-
-        //Add event listener to button
-        button.addEventListener('click', function () {
-            //Call showDetails function with the specific Pokemon object
-            showDetails(pokemon);
-        })
-    }
-
-    //Function to show details of the clicked Pokemon
-    function showDetails(pokemon) {
-        console.log(pokemon.name)
     }
 
     return {
         add: add,
         getAll: getAll,
-        addListItem: addListItem,
     };
 })();
-
-//Adding Pokemon to the repository
-pokemonRepository.add({ name: 'Abra', type: 'Psychic', height: 0.9, weight: 19.5 });
-
-//exercise 1.6 - DOM manipulation practice
-// function pokemonDetails(pokemonDetails) {
-// let pokemonButtons = document.querySelector('.pokemon-list');
-// document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
-
-//adds message if pokemon's height exceeds 1
-// if (pokemon.height > 1) {
-// document.write(' - Wow, that\'s a big one!');
-// }
-// document.write("<br>");
-// }
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-});
-
 
 //Display the list of Pokemon w/ heights on the DOM via 'for' loop
 // for (let i = 0; i < pokemonList.length; i++) {
@@ -110,17 +50,15 @@ pokemonRepository.getAll().forEach(function (pokemon) {
 //     document.write("<br>");
 // }
 
-
-
 //display list of pokemon names and heights using forEach method
-// function pokemonDetails(pokemon) {
-//     document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
+function pokemonDetails(pokemon) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
 
-//adds message if pokemon's height exceeds 1
-//     if (pokemon.height > 1) {
-//         document.write(' - Wow, that\'s a big one!');
-//     }
-//     document.write("<br>");
-// }
+    //adds message if pokemon's height exceeds 1
+    if (pokemon.height > 1) {
+        document.write(' - Wow, that\'s a big one!');
+    }
+    document.write("<br>");
+}
 
-// pokemonRepository.getAll().forEach(pokemon);
+pokemonRepository.getAll().forEach(pokemonDetails);
